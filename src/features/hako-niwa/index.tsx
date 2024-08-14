a3dcaaa647a3db5761a58606878d25c1f742bb66
yoshikouki <yoshikouki@pepabo.com>
2024-08-14 15:31:11 +0900

    Update Lights

--- src/features/hako-niwa/index.tsx
+++ src/features/hako-niwa/index.tsx
@@ -6,8 +6,19 @@ import { Canvas } from "@react-three/fiber";
 export const HakoNiwa = ({ children }: { children: React.ReactNode }) => {
   return (
     <Canvas camera={{ position: [0, 5, 10], fov: 75 }} gl={{ antialias: true }}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} intensity={1} />
+      <ambientLight intensity={0.01} />
+      <hemisphereLight intensity={0.125} color="#8040df" groundColor="red" />
+      <spotLight
+        castShadow
+        color="white"
+        intensity={20}
+        position={[-1, 3, 1]}
+        angle={64}
+        penumbra={1}
+        shadow-mapSize={[128, 128]}
+        shadow-bias={0.00005}
+      />
+
       <Bounds fit clip observe margin={1}>
         {children}
       </Bounds>
