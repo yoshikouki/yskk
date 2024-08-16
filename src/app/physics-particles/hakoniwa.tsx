f44f557da462c8f38cdf6c42d83dc01d0617e276
yoshikouki <yoshikouki@pepabo.com>
2024-08-16 12:22:43 +0900

    Update to 1,000 particles

--- src/app/physics-particles/hakoniwa.tsx
+++ src/app/physics-particles/hakoniwa.tsx
@@ -6,9 +6,46 @@ import {
   RoundedBox,
 } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { CuboidCollider, Physics, RigidBody } from "@react-three/rapier";
-import { Suspense } from "react";
+import {
+  CuboidCollider,
+  InstancedRigidBodies,
+  type InstancedRigidBodyProps,
+  Physics,
+  type RapierRigidBody,
+  RigidBody,
+} from "@react-three/rapier";
+import { Perf } from "r3f-perf";
+import { Suspense, useMemo, useRef } from "react";
 import * as THREE from "three";
+
+const COUNT = 1000;
+
+const ThousandsParticles = () => {
+  const points = useRef<RapierRigidBody[]>(null);
+
+  const instances = useMemo(() => {
+    const instances: InstancedRigidBodyProps[] = [];
+
+    for (let i = 0; i < COUNT; i++) {
+      instances.push({
+        key: `instance_${Math.random()}`,
+        position: [Math.random() - 1, Math.random() * 4, Math.random() - 1],
+        rotation: [Math.random(), Math.random(), Math.random()],
+      });
+    }
+
+    return instances;
+  }, []);
+
+  return (
+    <InstancedRigidBodies ref={points} instances={instances} colliders="ball">
+      <instancedMesh args={[undefined, undefined, COUNT]} count={COUNT}>
+        <sphereGeometry args={[0.01, 48, 48]} />
+        <meshToonMaterial color="#5786F5" />
+      </instancedMesh>
+    </InstancedRigidBodies>
+  );
+};
 
 export const PhysicsParticles = () => {
   return (
@@ -17,8 +54,10 @@ export const PhysicsParticles = () => {
       gl={{ antialias: true }}
       shadows
     >
+      <Perf position="bottom-left" />
+
       <Bounds fit clip observe margin={1}>
-        <Suspense>
+        <Suspense fallback={<>Building Physic World...</>}>
           <Physics
             gravity={[0, -1, 0]}
             // interpolation={false}
@@ -26,24 +65,18 @@ export const PhysicsParticles = () => {
             // debug
           >
             <RigidBody>
-              <RoundedBox args={[1, 1, 1]} castShadow receiveShadow>
+              <RoundedBox
+                args={[1, 1, 1]}
+                position={[0, 0.6, 0]}
+                castShadow
+                receiveShadow
+              >
                 <MeshTransmissionMaterial transmissionSampler />
               </RoundedBox>
             </RigidBody>
-            {Array.from({ length: 100 }).map((_, index) => (
-              <RigidBody
-                key={index}
-                position={[Math.random() * 2 - 1, 2, Math.random() * 2 - 1]}
-              >
-                <mesh>
-                  <sphereGeometry args={[0.05]} />
-                  <meshToonMaterial
-                    color={`hsl(${Math.random() * 360}, 100%, 50%)`}
-                  />
-                </mesh>
-              </RigidBody>
-            ))}
-            <CuboidCollider args={[2, 0.1, 2]} position={[0, -1, 0]}>
+            <ThousandsParticles />
+
+            <CuboidCollider args={[5, 0.1, 5]} position={[0, -1, 0]}>
               <meshToonMaterial side={THREE.DoubleSide} color="#ddd" />
             </CuboidCollider>
           </Physics>
