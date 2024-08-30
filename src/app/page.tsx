cc458a1f3d7fdf73771b379dbff684dfd0a220f2
yoshikouki <yoshikouki@pepabo.com>
2024-08-30 11:18:47 +0900

    Add links to top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,28 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { Footer } from "./footer";
-import { SnowGarden } from "./hakoniwa/snow-garden";
 import { Header } from "./header";
-import { RotatingCube } from "./rotating-cube";
 
 export default function HomePage() {
   return (
     <>
       <Header />
-      <main className="h-svh w-screen max-w-xl space-y-4 px-4 py-10">
-        <div className="aspect-square rounded border bg-card">
-          <SnowGarden />
-        </div>
-        <div className="aspect-square rounded border bg-card">
-          <RotatingCube />
-        </div>
+      <main className="flex w-screen flex-wrap items-center gap-4 px-4 py-10">
+        <Button asChild>
+          <Link href="/hakoniwa">Hakoniwa</Link>
+        </Button>
+        <Button asChild>
+          <Link href="/hakoniwa-box">Hakoniwa Box</Link>
+        </Button>
+        <Button asChild>
+          <Link href="/hakoniwa-snow">Hakoniwa Snow</Link>
+        </Button>
+        <Button asChild>
+          <Link href="/phaser-particles">Phaser Particles</Link>
+        </Button>
+        <Button asChild>
+          <Link href="/thousounds-particles">Thousand Sounds Particles</Link>
+        </Button>
       </main>
       <Footer />
     </>
